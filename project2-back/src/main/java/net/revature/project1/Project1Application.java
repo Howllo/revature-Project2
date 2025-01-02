ab1184bc34bf2e5483effe3a13f3a136b7e79294
Jibril Richter <jibrilrichter@gmail.com>
2025-01-02 08:41:05 -0600

    connected to database

--- project2-back/src/main/java/net/revature/project1/Project1Application.java
+++ project2-back/src/main/java/net/revature/project1/Project1Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "net.revature.project1.repository")
+@EnableJpaRepositories
 public class Project1Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 	}
+
 }
