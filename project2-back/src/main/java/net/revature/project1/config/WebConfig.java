e27c594f436365c3fea68777a653179e710836fd
Tony <allenr64y@ymail.com>
2025-01-01 22:12:42 -0600

    Fixed CORS

--- project2-back/src/main/java/net/revature/project1/config/WebConfig.java
+++ project2-back/src/main/java/net/revature/project1/config/WebConfig.java
@@ -1,10 +1,17 @@
 package net.revature.project1.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
