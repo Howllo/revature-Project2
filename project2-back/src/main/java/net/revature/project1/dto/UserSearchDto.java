98803c3ca08b01083ddfbb759d8f559a717eca98
Logan Sobrilsky <117660133+Shadalarus@users.noreply.github.com>
2025-01-03 11:57:08 -0500

    Update UserSearchDto.java

--- project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
+++ project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
@@ -1,4 +1,4 @@
 package net.revature.project1.dto;
 
-public record UserSearchDto(String username, String displayName, String profilePic, String bannerPic, String biography,
+public record UserSearchDto(Long id, String username, String displayName, String profilePic, String bannerPic, String biography,
                       Integer followerCount, Integer followingCount) { }
