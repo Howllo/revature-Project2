0cdf3e53ee86821a3ba49b8a01668d01c1972905
Logan Sobrilsky <117660133+Shadalarus@users.noreply.github.com>
2025-01-03 11:55:01 -0500

    Add files via upload

--- project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
+++ project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
@@ -1,3 +1,4 @@
 package net.revature.project1.dto;
 
-public record UserSearchDto(Long id, String username, String displayName, String profilePic) { }
+public record UserSearchDto(String username, String displayName, String profilePic, String bannerPic, String biography,
+                      Integer followerCount, Integer followingCount) { }
