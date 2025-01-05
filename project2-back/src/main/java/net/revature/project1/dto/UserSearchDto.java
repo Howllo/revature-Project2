7062ce45aaac70be8bc23b598ec3fbafca750d0f
Tony <allenr64y@ymail.com>
2025-01-05 10:33:08 -0600

    The update to UserSearchDto broke the JPA. Use UserDTO instead.

--- project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
+++ project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
@@ -1,6 +1,3 @@
 package net.revature.project1.dto;
 
-import java.sql.Timestamp;
-
-public record UserSearchDto(Long id, String username, String displayName, String profilePic, String bannerPic, String biography,
-                            Integer followerCount, Integer followingCount,Timestamp joinDate) { }
+public record UserSearchDto(Long id, String username, String displayName, String profilePic) { }
