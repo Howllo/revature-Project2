4c5b5046dd711a6c4be991cdf3d95d8468737aa3
Logan Sobrilsky <117660133+Shadalarus@users.noreply.github.com>
2025-01-06 15:18:12 -0500

    Add files via upload

--- project2-front/src/component/UserProfile/UserDisplay.jsx
+++ project2-front/src/component/UserProfile/UserDisplay.jsx
@@ -3,32 +3,61 @@ import UserAvatar from "../AvatarComponent/UserAvatar.jsx";
 
 const UserDisplay = ({user}) => {
     console.log(user)
-    return (
-        <Box>
+    if(user.bannerPic == null){
+        return (
             <Box>
-                <img
-                    //src={'https://picsum.photos/1500/500'}
-                    src={user.bannerPic}
-                    alt="Post Image"
-                    loading={"lazy"}
-                    style={{
-                        marginTop: '10px',
-                        width: '100%',
-                        height: '100%',
-                        borderRadius: '0px',
-                    }}
-                />
-                <Box
-                    sx={{
-                        mt: '-41px',
-                        marginLeft: '12px',
-                    }}
-                >
-                    <UserAvatar username={user.username} image={user.profilePic} width={64} height={64}/>
+                <Box>
+                    <img
+                        src={'https://picsum.photos/1500/500'}
+                        alt="Post Image"
+                        loading={"lazy"}
+                        style={{
+                            marginTop: '10px',
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: '0px',
+                        }}
+                    />
+                    <Box
+                        sx={{
+                            mt: '-41px',
+                            marginLeft: '12px',
+                        }}
+                    >
+                        <UserAvatar username={user.username} image={user.profilePic} width={64} height={64}/>
+                    </Box>
                 </Box>
             </Box>
-        </Box>
-    )
+        )
+    }
+    else{
+        return (
+            <Box>
+                <Box>
+                    <img
+                        src={user.bannerPic}
+                        alt="Post Image"
+                        loading={"lazy"}
+                        style={{
+                            marginTop: '10px',
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: '0px',
+                        }}
+                    />
+                    <Box
+                        sx={{
+                            mt: '-41px',
+                            marginLeft: '12px',
+                        }}
+                    >
+                        <UserAvatar username={user.username} image={user.profilePic} width={64} height={64}/>
+                    </Box>
+                </Box>
+            </Box>
+        )
+    }
+    
 }
 
 export default UserDisplay;
