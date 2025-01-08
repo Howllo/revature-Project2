3d301782d62590566a6b8c15045a72f8f776eea5
Logan Sobrilsky <117660133+Shadalarus@users.noreply.github.com>
2025-01-08 08:30:42 -0500

    Add files via upload

--- project2-front/src/component/UserProfile/UserDisplay.jsx
+++ project2-front/src/component/UserProfile/UserDisplay.jsx
@@ -2,13 +2,11 @@
 import UserAvatar from "../AvatarComponent/UserAvatar.jsx";
 
 const UserDisplay = ({user}) => {
-    console.log(user)
-    if(user.bannerPic == null){
         return (
             <Box>
                 <Box>
                     <img
-                        src={'https://picsum.photos/1500/500'}
+                        src={user.bannerPic || 'https://picsum.photos/1500/500'}
                         alt="Post Image"
                         loading={"lazy"}
                         style={{
@@ -29,34 +27,6 @@ const UserDisplay = ({user}) => {
                 </Box>
             </Box>
         )
-    }
-    else{
-        return (
-            <Box>
-                <Box>
-                    <img
-                        src={user.bannerPic}
-                        alt="Post Image"
-                        loading={"lazy"}
-                        style={{
-                            marginTop: '10px',
-                            width: '100%',
-                            height: '100%',
-                            borderRadius: '0px',
-                        }}
-                    />
-                    <Box
-                        sx={{
-                            mt: '-41px',
-                            marginLeft: '12px',
-                        }}
-                    >
-                        <UserAvatar username={user.username} image={user.profilePic} width={64} height={64}/>
-                    </Box>
-                </Box>
-            </Box>
-        )
-    }
     
 }
 
