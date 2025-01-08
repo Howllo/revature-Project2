fffe683f18c63d56dbc5f29a06eee633bba3aa52
Jibril Richter <jibrilrichter@gmail.com>
2025-01-07 18:42:37 -0600

    commit during rebase 6:42 1/7/2025

--- project2-back/src/test/java/SearchServiceTest.java
+++ project2-back/src/test/java/SearchServiceTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import net.revature.project1.Project1Application;
 import net.revature.project1.dto.UserSearchDto;
 import net.revature.project1.service.SearchService;
@@ -47,5 +48,24 @@ public class SearchServiceTest {
 //Asser
         Assertions.assertThat(userSearchDtos).isNotNull();
 
+=======
+package unittests;
+
+import net.revature.project1.service.SearchService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SearchServiceTest {
+
+    @Autowired
+    SearchService searchService;
+
+    @Test
+    public void contextLoad(){
+        Assertions.assertThat(searchService).isNot(null);
+>>>>>>> a4de21a (add SearchServiceTest)
     }
 }
