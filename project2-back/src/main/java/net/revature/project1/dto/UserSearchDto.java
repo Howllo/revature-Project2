6efaef11a1114b4dfcee2f3e3b3c59b99ccac2c0
Jibril Richter <jibrilrichter@gmail.com>
2025-01-07 18:36:47 -0600

    commit during rebase

--- project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
+++ project2-back/src/main/java/net/revature/project1/dto/UserSearchDto.java
@@ -1,3 +1,5 @@
 package net.revature.project1.dto;
 
-public record UserSearchDto(Long id, String username, String displayName, String profilePic) { }
+public record UserSearchDto(Long id, String username, String displayName, String profilePic) {
+
+}
