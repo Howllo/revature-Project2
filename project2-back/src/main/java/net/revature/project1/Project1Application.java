32baeb8eac36906c4976397f597a79901908ad63
Tony Hardiman Jr. <allenr64y@ymail.com>
2025-01-09 07:53:34 -0600

    Update Project1Application.java

--- project2-back/src/main/java/net/revature/project1/Project1Application.java
+++ project2-back/src/main/java/net/revature/project1/Project1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackages = "net.revature.project1.repository")
 public class Project1Application {
 
 	public static void main(String[] args) {
