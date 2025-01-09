fbefd6b927229eeedfb54141de0ffb3d7dc2b9eb
Tony <allenr64y@ymail.com>
2025-01-09 08:10:05 -0600

    Random commit to pull main.

--- project2-back/src/main/java/net/revature/project1/result/PostResult.java
+++ project2-back/src/main/java/net/revature/project1/result/PostResult.java
@@ -1,8 +1,6 @@
 package net.revature.project1.result;
 
 import net.revature.project1.dto.PostResponseDto;
-import net.revature.project1.entity.Post;
 import net.revature.project1.enumerator.PostEnum;
 
-public record PostResult(PostEnum postEnum, String message, PostResponseDto post) {
-}
+public record PostResult(PostEnum postEnum, String message, PostResponseDto post) { }
