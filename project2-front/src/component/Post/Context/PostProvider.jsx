c25e9b0bd1c99365ef89f2a5031cd7985d2816c7
Tony <allenr64y@ymail.com>
2025-01-11 11:28:06 -0600

    Removed debug since it working again.

--- project2-front/src/component/Post/Context/PostProvider.jsx
+++ project2-front/src/component/Post/Context/PostProvider.jsx
@@ -95,8 +95,6 @@ export const PostProvider = ({ children }) => {
                 media: mediaString
             };
 
-            console.log('Post payload:', JSON.stringify(postPayload));
-
             const token = Cookies.get('jwt');
             if (!token) {
                 throw new Error('No authentication token found');
