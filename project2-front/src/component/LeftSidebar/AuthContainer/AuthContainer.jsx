541d27415b732d3b85c0c714dbbf1237dc90a9a3
Jibril Richter <jibrilrichter@gmail.com>
2025-01-10 19:59:59 -0600

    commit after fixing the bugs in the profile page

--- project2-front/src/component/LeftSidebar/AuthContainer/AuthContainer.jsx
+++ project2-front/src/component/LeftSidebar/AuthContainer/AuthContainer.jsx
@@ -1,31 +1,37 @@
-﻿import {Box} from "@mui/material";
+﻿import { Box } from "@mui/material";
 import UserAvatar from "../../AvatarComponent/UserAvatar.jsx";
 import Cookies from "js-cookie";
 import Navbar from "../../Navbar/Navbar.jsx";
 
 const AuthContainer = () => {
-    return (
-        <Box
-            sx={{
-                display: 'flex',
-                width: '100%',
-                flexDirection: 'column',
-                justifyContent: 'flex-end',
-                alignItems: 'flex-end',
-                paddingRight: '20px',
-            }}
-        >
-            <Box
-                sx={{
-                    paddingBottom: '10px',
-                    paddingRight: '42px',
-                }}
-            >
-                <UserAvatar username={Cookies.get('username')} image={Cookies.get('profile_pic')}/>
-            </Box>
-            <Navbar/>
-        </Box>
-    )
-}
+  console.log("AuthContainer");
+  console.log(Cookies.get("username"));
+  console.log(Cookies.get("profile_pic"));
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
+        paddingRight: "20px",
+      }}
+    >
+      <Box
+        sx={{
+          paddingBottom: "10px",
+          paddingRight: "42px",
+        }}
+      >
+        <UserAvatar
+          username={Cookies.get("username")}
+          image={Cookies.get("profile_pic")}
+        />
+      </Box>
+      <Navbar />
+    </Box>
+  );
+};
 
 export default AuthContainer;
--- project2-front/src/component/UserProfile/UserDisplay.jsx
+++ project2-front/src/component/UserProfile/UserDisplay.jsx
@@ -1,12 +1,13 @@
 ﻿import { Box } from "@mui/material";
 import UserAvatar from "../AvatarComponent/UserAvatar.jsx";
+import Cookies from "js-cookie";
 
 const UserDisplay = ({ user }) => {
   return (
     <Box>
       <Box>
         <img
-          src={user.bannerPic || "https://picsum.photos/1500/500"}
+          src={Cookies.get("banner_pic") || "https://picsum.photos/1500/500"}
           alt="Post Image"
           loading={"lazy"}
           style={{
@@ -17,19 +18,18 @@ const UserDisplay = ({ user }) => {
             maxWidth: "546px",
             maxHeight: "180px",
             minWidth: "546px",
-            minHeight: "180px"
+            minHeight: "180px",
           }}
         />
         <Box
           sx={{
             mt: "-46px",
-            ml: "-30px"
-            
+            ml: "-30px",
           }}
         >
           <UserAvatar
-            username={user.username}
-            image={user.profilePic}
+            username={Cookies.get("username")}
+            image={Cookies.get("profile_pic")}
             width={64}
             height={64}
           />
