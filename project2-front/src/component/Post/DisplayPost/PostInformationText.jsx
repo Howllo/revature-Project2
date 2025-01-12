36350358f97977b0ed0576f3f07c5ba08b45b213
Tony <allenr64y@ymail.com>
2025-01-12 13:47:54 -0600

    Random fixes.

--- project2-front/src/component/Post/DisplayPost/PostInformationText.jsx
+++ project2-front/src/component/Post/DisplayPost/PostInformationText.jsx
@@ -1,21 +1,7 @@
-﻿import {Box, IconButton, Menu, Typography} from "@mui/material";
+﻿import {Box, Typography} from "@mui/material";
 import PropTypes from 'prop-types';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import MenuItem from "@mui/material/MenuItem";
-import {useEffect, useState} from "react";
-import Cookies from "js-cookie";
-import useAuth from "../../../util/auth/UseAuth.jsx";
-import {usePost} from "../Context/UsePost.jsx";
-import EditPostDialog from "./EditPostDialog.jsx";
 
-const PostText = ({display_name, username, post_date, post}) => {
-    const {user} = useAuth()
-    const {deletePost} = usePost();
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const [handleDropdownMenu, setHandleDropdownMenu] = useState(false);
-    const [openEditBox, setOpenEditBox] = useState(false);
-
+const PostText = ({display_name, username, post_date}) => {
     const daysBetween = () => {
         if (!post_date) return '0m';
 
@@ -40,32 +26,6 @@ const PostText = ({display_name, username, post_date, post}) => {
         } else {
             return `${years}y`;
         }
-    };
-
-    useEffect(() => {
-        handleCheckIfAuthorized()
-    }, [user]);
-
-    const handleCheckIfAuthorized = () => {
-        setHandleDropdownMenu(Cookies.get('user_id') === post.userId.toString())
-    }
-    
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleDeletePost = async () => {
-        await deletePost(post);
-        handleClose();
-    }
-
-    const handleEditPost = () => {
-        setOpenEditBox(true);
-        handleClose();
-    }
-
-    const handleClose = () => {
-        setAnchorEl(null);
     };
 
     return (
@@ -99,9 +59,9 @@ const PostText = ({display_name, username, post_date, post}) => {
                     fontFamily="Inter, sans-serif"
                     sx={{
                         paddingLeft: '5px',
-                        paddingTop: '4px',
+                        paddingTop: '2px',
                         flexDirection: 'row',
-                        fontSize: '12.125px',
+                        fontSize: '13px',
                         wordWrap: 'break-word',
                         overflow: 'hidden',
                         color: 'rgb(66, 87, 108)'
@@ -119,11 +79,10 @@ const PostText = ({display_name, username, post_date, post}) => {
             >
                 <Typography
                     variant="h6"
-                    fontFamily="Inter, sans-serif"
                     sx={{
                         display: 'flex',
                         paddingLeft: '5px',
-                        paddingTop: '2px',
+                        paddingTop: '1px',
                         flexDirection: 'row',
                         fontSize: '15px',
                         wordWrap: 'break-word',
@@ -138,11 +97,10 @@ const PostText = ({display_name, username, post_date, post}) => {
             <Box>
                 <Typography
                     variant="h6"
-                    fontFamily="Inter, sans-serif"
                     sx={{
                         display: 'flex',
                         paddingLeft: '5px',
-                        paddingTop: '3px',
+                        paddingTop: '2px',
                         flexDirection: 'row',
                         fontSize: '13.125px',
                         fontWeight: 600,
@@ -154,37 +112,6 @@ const PostText = ({display_name, username, post_date, post}) => {
                     {daysBetween()}
                 </Typography>
             </Box>
-
-            {handleDropdownMenu ? <Box
-                sx={{
-                    display: 'flex',
-                    alignSelf: 'flex-end',
-                }}
-            >
-                <IconButton
-                    id="basic-button"
-                    aria-controls={open ? 'basic-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onClick={handleClick}
-                >
-                    <MoreVertIcon/>
-                </IconButton>
-                <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                        'aria-labelledby': 'basic-button',
-                    }}
-                >
-                    <MenuItem onClick={handleEditPost}>Edit</MenuItem>
-                    <MenuItem onClick={handleDeletePost}>Delete</MenuItem>
-                </Menu>
-            </Box> : null}
-
-            {openEditBox ? <EditPostDialog open={openEditBox} setOpen={setOpenEditBox} post={post}/> : null}
         </Box>
     )
 }
