e11d3cd72fc24759fd7ebc4d2c0935ca20468843
Logan Sobrilsky <117660133+Shadalarus@users.noreply.github.com>
2025-01-13 15:56:33 -0500

    Add files via upload

--- project2-front/src/page/SettingPage.jsx
+++ project2-front/src/page/SettingPage.jsx
@@ -1,27 +1,31 @@
 ﻿import { Box, Button, InputAdornment, OutlinedInput } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom"
+import useNav from "../component/Navbar/NavContext/UseNav.jsx";
 import EmailIcon from "@mui/icons-material/Email";
 import LogOut from "../component/LogOut/LogOut.jsx";
-
+import Cookies from "js-cookie";
 import { useState } from "react";
 import { projectApi } from "../util/axios.js";
 import "./SettingPage.css"
 
 const SettingsPage = () => {
-  const [displayName, setDisplayName] = useState();
+  const [username, setUsername] = useState();
+  const {currentNav} = useNav();
+    let navigate  = useNavigate();
 
-  const changeName = async (username) => {
-    if (displayName === undefined) {
+  const changeName = async (id) => {
+    const token = Cookies.get("jwt");
+    if (username === undefined) {
       return [];
     }
 
     try {
       const response = await projectApi.put(
-        `/user/${username}/display_name`,
-        {
-          displayName: displayName,
-        },
+        `/user/${id}/update/${username}`,
+        
         {
           headers: {
+            Authorization: `Bearer ${token}`,
             "Content-Type": "application/json",
           },
         }
@@ -34,26 +38,30 @@ const SettingsPage = () => {
   };
 
   const handleDisplayNameChange = (e) => {
-    setDisplayName(e.target.value);
+    setUsername(e.target.value);
   };
 
   const handleDisplayChange = async () => {
-    await changeName(displayName);
+    await changeName(Cookies.get("user_id"));
+    Cookies.remove('jwt')
+    Cookies.remove('username')
+    navigate('/')
+    window.location.reload(true)
   };
 
   return (
     <Box className="SettingPageContainer">
       <OutlinedInput className="SettingPageInputBar text" required type="text"
-        value={displayName}
+        value={username}
         onChange={(e) => handleDisplayNameChange(e)}
-        placeholder="Enter new display name"
+        placeholder="Enter new username"
         startAdornment={
           <InputAdornment position="start">
             <EmailIcon />
           </InputAdornment>
         }
       />
-
+      <p>Note: Changing your username will log you out.</p>
       <Button className="SettingPageChangeNameButton" variant="contained" onClick={handleDisplayChange}>
         Change Name
       </Button>
