ee8fe5caad0c47b8ae8af390f5fa0c07b8dd32fb
Logan Sobrilsky <117660133+Shadalarus@users.noreply.github.com>
2025-01-14 17:24:52 -0500

    Add files via upload

---
+++ project2-front/src/component/UsernameChange/changeUsernameValidation.js
@@ -0,0 +1,33 @@
+﻿
+import { projectApi } from "../../util/axios";
+import { RequirementsUsername } from "../../util/RequirementsAccount.js";
+
+export const changeUsernameValidation = (username) => {
+
+  const validateAll = async () => {
+    const isUsernameAllowed = await checkUsername();
+    const isValidUsername = RequirementsUsername(username);
+    return !(!isValidUsername || !isUsernameAllowed);
+  };
+
+  const checkUsername = async () => {
+    try {
+      const response = await projectApi.get(
+        `/user/check/username/${username}`
+      );
+      return response.status === 200;
+    } catch (e) {
+      if (e.response.status === 403 || e.response.status === 500) {
+        console.log(e.response.status);
+      }
+      return false;
+    }
+  };
+
+  return {
+    checkUsername,
+    validateAll,
+  };
+};
+
+export default changeUsernameValidation;
