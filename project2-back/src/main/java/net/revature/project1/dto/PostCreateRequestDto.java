f033714f4108ebd337e48d5c68157c52ea48b5e3
Tony <allenr64y@ymail.com>
2025-01-15 08:58:40 -0600

    Fixing issue with video not uploading.

--- project2-back/src/main/java/net/revature/project1/dto/PostCreateRequestDto.java
+++ project2-back/src/main/java/net/revature/project1/dto/PostCreateRequestDto.java
@@ -1,5 +1,6 @@
 package net.revature.project1.dto;
 
+import jakarta.annotation.Nullable;
 import org.springframework.web.multipart.MultipartFile;
 
-public record PostCreateRequestDto(Long userId, Long postParent, String comment, MultipartFile media) { }
+public record PostCreateRequestDto(Long userId, @Nullable Long postParent, String comment, MultipartFile media) { }
